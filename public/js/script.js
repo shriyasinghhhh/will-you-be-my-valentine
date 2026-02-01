a71f457665766657c918462121134d662ccbbdf2
shriyasinghhhh <shriyasinghhhh@gmail.com>
2026-02-01 11:03:32 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -66,12 +66,6 @@ let size = 50;
 let clicks = 0;
 
 no_button.addEventListener('click', () => {
-    // Change banner source
-    let banner = document.getElementById('banner');
-    if (clicks === 0) {
-        banner.src = "public/images/no.gif";
-        refreshBanner();
-    }
     clicks++;
     // increase button height and width gradually to 250px
     const sizes = [40, 50, 30, 35, 45]
@@ -96,25 +90,20 @@ no_button.addEventListener('click', () => {
 });
 
 yes_button.addEventListener('click', () => {
-    // change banner gif path
-    let banner = document.getElementById('banner');
-    banner.src = "public/images/yes.gif";
-    refreshBanner();
+    
     // hide buttons div
     let buttons = document.getElementsByClassName('buttons')[0];
     buttons.style.display = "none";
     // show message div
     let message = document.getElementsByClassName('message')[0];
     message.style.display = "block";
+     // show & play video
+    let video = document.getElementById('banner');
+    video.style.display = "block";
+    video.currentTime = 0;
+    video.play();
 });
 
-function refreshBanner() {
-    // Reload banner gif to force load  
-    let banner = document.getElementById('banner');
-    let src = banner.src;
-    banner.src = '';
-    banner.src = src;
-}
 
 function changeLanguage() {
     const selectElement = document.getElementById("language-select");
